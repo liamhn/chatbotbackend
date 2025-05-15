d65cc8c8b9c1081b2a4baa63af35242e074b2304
Liam Haas-Neill <liam_hn@hotmail.ca>
2025-05-15 12:43:41 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import numpy as np
 import os
 
 # Load Groq API key from environment
-GROQ_API_KEY = os.getenv("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")
+GROQ_API_KEY = os.getenv("GROQ_SPI_KEY")
 
 app = FastAPI()
 
